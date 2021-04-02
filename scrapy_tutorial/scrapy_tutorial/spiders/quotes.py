35ba43aab46a47999101a2fcee45ef9c87a922a1
Parsa KamaliPour <parsakamalipour@gmail.com>
2021-04-02 20:06:41 +0430

    crawled all the pages & saved them as json

--- scrapy_tutorial/scrapy_tutorial/spiders/quotes.py
+++ scrapy_tutorial/scrapy_tutorial/spiders/quotes.py
@@ -15,3 +15,7 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
